c3bfe8c2d5d03366e17927455bddff09ea788835
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-08-15 00:10:15 +0100

    wip3

--- src/utils/rpn_helper.py
+++ src/utils/rpn_helper.py
@@ -45,6 +45,22 @@ def get_resized_img_size(width, height, resized_img_min_size):
     return new_width, new_height
 
 
+def compute_regr(anchor_box, gt_box):
+    # implement second part of equation 2 in Faster-RCNN paper
+    anchor_width = anchor_box[2] - anchor_box[0]
+    anchor_height = anchor_box[3] - anchor_box[1]
+    gt_center_x = 0.5 * (gt_box[0] + gt_box[2])
+    gt_center_y = 0.5 * (gt_box[1] + gt_box[3])
+    anchor_center_x = 0.5 * (anchor_box[0] + anchor_box[2])
+    anchor_center_y = 0.5 * (anchor_box[1] + anchor_box[3])
+
+    tx_star = (gt_center_x - anchor_center_x) / anchor_width
+    ty_star = (gt_center_y - anchor_center_y) / anchor_height
+    tw_star = np.log((gt_box[2] - gt_box[0]) / anchor_width)
+    th_star = np.log((gt_box[3] - gt_box[1]) / anchor_height)
+    return tx_star, ty_star, tw_star, th_star
+
+
 def compute_rpn_of_img(img_info, config, width, height, resized_width, resized_height, compute_feature_sizes):
     """
     
@@ -67,24 +83,25 @@ def compute_rpn_of_img(img_info, config, width, height, resized_width, resized_h
     n_bbs = len(img_info['bbox'])  # number of bboxes in this image
     best_iou_for_bb = np.zeros(n_bbs, dtype=np.float32)
 
+    # step 0.3 init some buffers
     best_anchor_for_bb = -1 * np.ones((n_bbs, 4), dtype=np.int32)
-
     best_corner_for_bb = np.ones((n_bbs, 4), dtype=np.int32)
-
     best_param_for_bb = np.ones((n_bbs, 4), dtype=np.float32)
-
     num_anchor_for_bb = np.zeros(n_bbs, dtype=np.int32)
 
-    # step 0.3: resize bbox:
+    # step 0.4: resize bbox:
     ground_truth_bb_list = get_bbox_list_resized(img_info['bbox'], width, height, resized_width, resized_height)
-
-
 
     # step 1: compute a number of anchors for each pixel in the output mapget_bbox_list_resized
     # step 1.1: get feature map size (after conv layers), note that image input is the resized one
     feat_width, feat_height = compute_feature_sizes(resized_width, resized_height)
 
-    # step 1.2: for each pixel in feature map, compute valid anchors
+    # step 1.2: init output objectives
+    y_rpn_overlap = np.zeros((feat_height, feat_width, n_anchors))  # at every pixel of feature map, consider n anchors
+    y_is_box_valid = np.zeros((feat_height, feat_width, n_anchors))
+    y_rpn_regr = np.zeros((feat_height, feat_width, n_anchors * 4))
+
+    # step 1.3: for each pixel in feature map, compute valid anchors
     for anchor_size_idx in range(len(anchor_sizes)):
         for anchor_ratio_idx in range(len(anchor_ratios)):
             anchor_width = anchor_sizes[anchor_size_idx] * anchor_ratios[anchor_ratio_idx][0]
@@ -106,6 +123,8 @@ def compute_rpn_of_img(img_info, config, width, height, resized_width, resized_h
 
                     best_iou_for_loc = 0.0  # for current location on feat map + current anchor configuration
                     # different from best IOU for a ground truth box
+                    # init best_regression for current location
+                    best_regression = (0.0, 0.0, 0.0, 0.0)  # not sure if initialize by 0. is a good idea
 
                     for bb_idx in range(n_bbs):
                         # compute iou of the current ground truth box vs the current anchor box
@@ -116,15 +135,7 @@ def compute_rpn_of_img(img_info, config, width, height, resized_width, resized_h
                         cur_iou = iou(cur_gt, cur_anchor)
                         if cur_iou > best_iou_for_bb[bb_idx] or cur_iou > config.upper_bound_iou:
                             # implement second part of equation 2 in Faster-RCNN paper
-                            gt_center_x = 0.5 * (cur_gt[0] + cur_gt[2])
-                            gt_center_y = 0.5 * (cur_gt[1] + cur_gt[3])
-                            anchor_center_x = 0.5 * (cur_anchor[0] + cur_anchor[2])
-                            anchor_center_y = 0.5 * (cur_anchor[1] + cur_anchor[3])
-
-                            tx_star = (gt_center_x - anchor_center_x) / anchor_width
-                            ty_star = (gt_center_y - anchor_center_y) / anchor_height
-                            tw_star = np.log((cur_gt[2] - cur_gt[0]) / anchor_width)
-                            th_star = np.log((cur_gt[3] - cur_gt[1]) / anchor_height)
+                            tx_star, ty_star, tw_star, th_star = compute_regr(cur_anchor, cur_gt)
 
                             # anchor is considered positive only when it's > upper bound
                             if cur_iou > config.upper_bound_iou:
@@ -142,6 +153,21 @@ def compute_rpn_of_img(img_info, config, width, height, resized_width, resized_h
                                 best_iou_for_bb[bb_idx] = cur_iou
                                 best_corner_for_bb[bb_idx] = [anchor_xmin, anchor_xmax, anchor_ymin, anchor_ymax]
                                 best_param_for_bb[bb_idx] = [tx_star, ty_star, tw_star, th_star]
+
+                    n_anchor_ratios = len(config.anchor_ratios)
+                    if anchor_type == 'negative':
+                        y_is_box_valid[feat_y, feat_x, anchor_ratio_idx + n_anchor_ratios * anchor_size_idx] = 1
+                        y_rpn_overlap[feat_y, feat_x, anchor_ratio_idx + n_anchor_ratios * anchor_size_idx] = 0
+                    elif anchor_type == 'positive':
+                        y_is_box_valid[feat_y, feat_x, anchor_ratio_idx + n_anchor_ratios * anchor_size_idx] = 1
+                        y_rpn_overlap[feat_y, feat_x, anchor_ratio_idx + n_anchor_ratios * anchor_size_idx] = 1
+                        start_idx = 4 * (anchor_ratio_idx + n_anchor_ratios * anchor_size_idx)
+                        y_rpn_regr[feat_y, feat_x, start_idx: start_idx + 4] = best_regression
+                    else:
+                        y_is_box_valid[feat_y, feat_x, anchor_ratio_idx + n_anchor_ratios * anchor_size_idx] = 0
+                        y_rpn_overlap[feat_y, feat_x, anchor_ratio_idx + n_anchor_ratios * anchor_size_idx] = 0
+
+
 
 
 
