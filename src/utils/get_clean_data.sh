9662e1c2f399a3fd69ebc332541b9906b01032bb
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-08-15 20:37:19 +0100

    make clean directory when it is not present

--- src/utils/get_clean_data.sh
+++ src/utils/get_clean_data.sh
@@ -3,11 +3,8 @@ data_path=/data/hav16/imagenet
 clean_data_path=/data/hav16/clean
 clean_file_path=${clean_data_path}/imagenet_clean.zip
 
-if [ ! -d "$data_path" ]
-then
-    mkdir -p $data_path
-    chmod 700 $data_path
-fi
+mkdir -p $data_path
+mkdir -p $clean_data_path
 
 
 if [ ! -f "$clean_file_path" ]
@@ -15,4 +12,4 @@ then
     wget -O ${clean_file_path} https://www.dropbox.com/s/39e7kbukhgltd9t/clean_imagenet.zip?dl=0
 fi
 
-unzip $clean_file_path -d $data_path
+unzip -q $clean_file_path -d $data_path
